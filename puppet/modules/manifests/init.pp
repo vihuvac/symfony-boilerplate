180e87d38097e44d802c8153e78c01926502e295
Víctor Hugo Valle Castillo <victouk@gmail.com>
2015-07-15 12:48:18 -0300

    updating text indentation into files

--- puppet/modules/manifests/init.pp
+++ puppet/modules/manifests/init.pp
@@ -1,29 +1,30 @@
 class dotdeb {
 
-  class { 'apt': }
+    class { "apt" : }
 
-  apt::source { 'dotdeb':
-    location   => 'http://packages.dotdeb.org',
-    release    => 'squeeze',
-    repos      => 'all',
-    key        => 'dotdeb',
-    key_source => 'http://www.dotdeb.org/dotdeb.gpg',
-  }
+    apt::source { "dotdeb" :
+        location   => "http://packages.dotdeb.org",
+        release    => "squeeze",
+        repos      => "all",
+        key        => "dotdeb",
+        key_source => "http://www.dotdeb.org/dotdeb.gpg",
+    }
 
-  apt::source { 'dotdeb-php54':
-    location   => 'http://packages.dotdeb.org',
-    release    => 'squeeze-php54',
-    repos      => 'all',
-    key        => 'dotdeb',
-    key_source => 'http://www.dotdeb.org/dotdeb.gpg',
-  }
-  exec { "apt-update":
-      command => "/usr/bin/apt-get update"
-  }
+    apt::source { "dotdeb-php54" :
+        location   => "http://packages.dotdeb.org",
+        release    => "squeeze-php54",
+        repos      => "all",
+        key        => "dotdeb",
+        key_source => "http://www.dotdeb.org/dotdeb.gpg",
+    }
 
-  # Ensure apt-get update is run before package instalations
-  Exec["apt-update"] -> Package <| |>
+    exec { "apt-update" :
+        command => "/usr/bin/apt-get update"
+    }
 
-  # Ensure sources are installed before running apt-get update
-  Apt::Source <| |> -> Exec["apt-update"]
+    # Ensure apt-get update is run before package instalations
+    Exec["apt-update"] -> Package <| |>
+
+    # Ensure sources are installed before running apt-get update
+    Apt::Source <| |> -> Exec["apt-update"]
 }
--- puppet/modules/php/manifests/pear.pp
+++ puppet/modules/php/manifests/pear.pp
@@ -1,23 +1,23 @@
 # TODO REWRITE THIS USING A CUSTOM TYPE+PROVIDER
 class php::pear inherits php::params {
 
-  package { 'pear':
-    name => $pear_package_name,
-    ensure => present,
-    require => Class['php'],
-  }
+    package { "pear" :
+        name    => $pear_package_name,
+        ensure  => present,
+        require => Class["php"],
+    }
 
-  # upgrades PEAR, installs basic PHP tools
-  exec { "pear_upgrade":
-    command => "pear upgrade",
-    require => Package['pear'],
-    returns => [ 0, '', ' '],
-  }
+    # upgrades PEAR, installs basic PHP tools
+    exec { "pear_upgrade" :
+        command => "pear upgrade",
+        require => Package["pear"],
+        returns => [ 0, "", " "],
+    }
 
-  exec { "pear_auto_discover":
-    command => "pear config-set auto_discover 1",
-    require => [Package['pear'], Exec['pear_upgrade']],
-  }
+    exec { "pear_auto_discover" :
+        command => "pear config-set auto_discover 1",
+        require => [Package["pear"], Exec["pear_upgrade"]],
+    }
 
     # Moved to its own module (phpunit directory) because the installation through pear is deprecated!
     #exec { "phpunit" :

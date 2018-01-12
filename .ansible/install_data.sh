903f467893d731460122a782fb84f126e22551f9
Víctor Hugo Valle Castillo <victouk@gmail.com>
2018-01-12 20:33:45 -0300

    default development domain name updated (due to chrome legacy update).

--- .ansible/install_data.sh
+++ .ansible/install_data.sh
@@ -7,13 +7,13 @@ else
 	echo "This is not a Symfony project so the file 'bin/load_data.php' does not exist."
 fi
 
-curl -s -H "Host: site.symfony-boilerplate.dev" http://192.168.33.11/app_dev.php | grep 3e9fda56df2cdd3b039f189693ab7844fbb2d4f6
+curl -s -H "Host: dev.symfony-boilerplate" http://192.168.33.11/app_dev.php | grep 3e9fda56df2cdd3b039f189693ab7844fbb2d4f6
 
 echo ""
 echo "Congratulation!!!"
 echo "You have installed a virtual machine for the Project."
 echo ""
 echo "You need to :"
-echo "    - add site.symfony-boilerplate.dev to point 192.168.33.11 in your hosts file."
+echo "    - add dev.symfony-boilerplate to point 192.168.33.11 in your hosts file."
 echo "    - start hacking around ;)"
 echo ""
